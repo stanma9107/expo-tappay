f24cbdc67880ce767b3b75cad3949ae79f4d0f3d
Stan <lol97554@gmail.com>
2023-11-28 22:12:51 +0800

    chore: make to fitting example.

--- example/App.tsx
+++ example/App.tsx
@@ -1,11 +1,10 @@
-import { StyleSheet, Text, View } from 'react-native';
-
-import * as ExpoTappay from 'expo-tappay';
+import * as ExpoTappay from "expo-tappay";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>{ExpoTappay.hello()}</Text>
+      <Text>{ExpoTappay.getTheme()}</Text>
     </View>
   );
 }
@@ -13,8 +12,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,7 @@
-import { NativeModulesProxy, EventEmitter, Subscription } from 'expo-modules-core';
-
 // Import the native module. On web, it will be resolved to ExpoTappay.web.ts
 // and on native platforms to ExpoTappay.ts
-import ExpoTappayModule from './ExpoTappayModule';
-import ExpoTappayView from './ExpoTappayView';
-import { ChangeEventPayload, ExpoTappayViewProps } from './ExpoTappay.types';
+import ExpoTappayModule from "./ExpoTappayModule";
 
-// Get the native constant value.
-export const PI = ExpoTappayModule.PI;
-
-export function hello(): string {
-  return ExpoTappayModule.hello();
+export function getTheme(): string {
+  return ExpoTappayModule.getTheme();
 }
-
-export async function setValueAsync(value: string) {
-  return await ExpoTappayModule.setValueAsync(value);
-}
-
-const emitter = new EventEmitter(ExpoTappayModule ?? NativeModulesProxy.ExpoTappay);
-
-export function addChangeListener(listener: (event: ChangeEventPayload) => void): Subscription {
-  return emitter.addListener<ChangeEventPayload>('onChange', listener);
-}
-
-export { ExpoTappayView, ExpoTappayViewProps, ChangeEventPayload };
