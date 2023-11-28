dbae1ed5c16fb3c60b383e0f715021e34570d2c1
Stan <lol97554@gmail.com>
2023-11-28 22:06:42 +0800

    chore: remove native view.

--- android/src/main/java/dev/stanma/tappay/ExpoTappayView.kt
+++
@@ -1,7 +0,0 @@
-package dev.stanma.tappay
-
-import android.content.Context
-import expo.modules.kotlin.AppContext
-import expo.modules.kotlin.views.ExpoView
-
-class ExpoTappayView(context: Context, appContext: AppContext) : ExpoView(context, appContext)
