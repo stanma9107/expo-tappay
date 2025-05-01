403b56e648ab26594d60d1e3f41ee343fed768b1
Stan <lol97554@gmail.com>
2025-05-01 17:05:20 +0800

    feat(android): setup Tappay.

--- android/src/main/java/dev/stanma/tappay/ExpoTappayModule.kt
+++ android/src/main/java/dev/stanma/tappay/ExpoTappayModule.kt
@@ -1,3 +1,7 @@
+// Copyright         : 2025 Stan Ma
+// File Name         : ExpoTappay/android/src/main/java/dev/stanma/tappay/ExpoTappayModule.kt
+// Description       : Expo Module to integrate with Third-Party Payment Service Provider (Tappay)
+
 package dev.stanma.tappay
 
 import android.content.Context
@@ -7,19 +11,22 @@ import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
 import tech.cherri.tpdirect.api.TPDLinePay
 import expo.modules.kotlin.exception.Exceptions
+import expo.modules.kotlin.types.Enumerable
+import tech.cherri.tpdirect.api.TPDServerType
+import tech.cherri.tpdirect.api.TPDSetup
 
 class ExpoTappayModule : Module() {
 
-  private lateinit var context: Context;
-  private var applicationInfo: ApplicationInfo? = null;
+  private lateinit var context: Context
+  private var applicationInfo: ApplicationInfo? = null
   private var supportPayments: List<String> = emptyList()
 
   override fun definition() = ModuleDefinition {
     Name("ExpoTappay")
 
     OnCreate {
-      context = appContext.reactContext ?: throw Exceptions.ReactContextLost();
-      applicationInfo = context.packageManager?.getApplicationInfo(context.packageName.toString(), PackageManager.GET_META_DATA);
+      context = appContext.reactContext ?: throw Exceptions.ReactContextLost()
+      applicationInfo = context.packageManager?.getApplicationInfo(context.packageName.toString(), PackageManager.GET_META_DATA)
       supportPayments = applicationInfo?.metaData?.getString("TPDSupportPayments")?.split(",") ?: emptyList()
     }
 
@@ -34,5 +41,24 @@ class ExpoTappayModule : Module() {
     Function("isLinePayAvailable") {
       return@Function supportPayments.contains("linePay") && TPDLinePay.isLinePayAvailable(appContext.reactContext)
     }
+
+    Function("setup") { appId: Int, appKey: String, serverType: ServerType ->
+      TPDSetup.initInstance(
+        context,
+        appId,
+        appKey,
+        when (serverType) {
+          ServerType.SANDBOX -> TPDServerType.Sandbox
+          ServerType.PRODUCTION -> TPDServerType.Production
+        }
+      )
+    }
+
+
   }
+}
+
+enum class ServerType(val value: String) : Enumerable {
+  SANDBOX("sandbox"),
+  PRODUCTION("production")
 }
