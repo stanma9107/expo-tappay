77392fb63ebeed587c0b7fd6cfe51809159d4342
miiia.yang <i0987801999@gmail.com>
2025-07-31 03:43:14 +0800

    fix: update docusaurus config for correct GitHub Pages deployment

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -15,14 +15,14 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://miiiayang.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/expo-tappay/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'stanma9107', // Usually your GitHub org/user name.
+  organizationName: 'miiiayang', // Usually your GitHub org/user name.
   projectName: 'expo-tappay', // Usually your repo name.
 
   onBrokenLinks: 'throw',
